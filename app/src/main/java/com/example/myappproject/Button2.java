c6cb74aeed926a944e90dd40fe283c9bab35f630
sallywilli <106054193+sallywilli@users.noreply.github.com>
2022-06-15 23:19:43 +0900

    Update Button2.java

--- app/src/main/java/com/example/myappproject/Button2.java
+++ app/src/main/java/com/example/myappproject/Button2.java
@@ -6,7 +6,7 @@ import androidx.annotation.Nullable;
 
 public class Button2 extends ThirdActivity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {  //third.xml에 있는 첫번째 버튼인 btn2을 누르면 btn2.xml 화면으로 이동합니다.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.btn2);
 

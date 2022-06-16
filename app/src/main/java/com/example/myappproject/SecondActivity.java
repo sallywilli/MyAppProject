2955765233058e4ad86fb7e2a502a5c1165b90ea
sallywilli <106054193+sallywilli@users.noreply.github.com>
2022-06-16 22:48:35 +0900

    Update SecondActivity.java

--- app/src/main/java/com/example/myappproject/SecondActivity.java
+++ app/src/main/java/com/example/myappproject/SecondActivity.java
@@ -22,7 +22,7 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second);
+        setContentView(R.layout.second);    //second.xml 화면을 보여줍니다
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.drawable.title);
@@ -35,17 +35,17 @@ public class SecondActivity extends AppCompatActivity {
         t5 = findViewById(R.id.t5);
         t6 = findViewById(R.id.t6);
 
-        insert.setOnClickListener(new View.OnClickListener() {
+        insert.setOnClickListener(new View.OnClickListener() {      //입력 버튼 클릭 시 큰 사이즈 메시지 창 팝업
             @Override
             public void onClick(View view) {
                 dialogView = View.inflate(SecondActivity.this, R.layout.second, null);
                 AlertDialog.Builder dlg = new AlertDialog.Builder(SecondActivity.this);
-                dlg.setTitle("내 정보 입력");
+                dlg.setTitle("내 정보 입력");    //메시지창 제목
                 dlg.setIcon(R.drawable.icon);
                 dlg.setView(dialogView);
-                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {     //확인 버튼 클릭 시 각각의 에디트텍스트에 입력한 정보가 화면에 저장되어 보여짐
                     @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
+                    public void onClick(DialogInterface dialogInterface, int i) {       //에디트텍스트에 입력한 정보들이 본화면의 텍스트뷰에 동일하게 옮겨 보여짐
                         et1 = dialogView.findViewById(R.id.et1);
                         et2 = dialogView.findViewById(R.id.et2);
                         et3 = dialogView.findViewById(R.id.et3);
@@ -60,11 +60,11 @@ public class SecondActivity extends AppCompatActivity {
                         t6.setText("집 전화번호: " + et6.getText().toString());
                     }
                 });
-                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {     //취소 버튼 클릭 시
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         Toast toast = new Toast(SecondActivity.this);
-                        toast.setText("취소했습니다");
+                        toast.setText("취소했습니다");        //토스트 내용
                         toast.show();
                     }
                 });

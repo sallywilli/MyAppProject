8be7df48cd7bba767851825eb7468fc18022f78e
sallywilli <106054193+sallywilli@users.noreply.github.com>
2022-06-16 22:29:22 +0900

    Update Button5.java

--- app/src/main/java/com/example/myappproject/Button5.java
+++ app/src/main/java/com/example/myappproject/Button5.java
@@ -13,7 +13,7 @@ public class Button5 extends ThirdActivity{
     Button finish;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {  //third.xml에 있는 다섯번째 버튼인 btn5을 누르면 btn5.xml 화면으로 이동합니다.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.btn5);
 
@@ -26,8 +26,8 @@ public class Button5 extends ThirdActivity{
             @Override
             public void onClick(View view) {
                 AlertDialog.Builder dlg = new AlertDialog.Builder(Button5.this);
-                dlg.setTitle("회원탈퇴");
-                dlg.setMessage("이용해주셔서 감사합니다");
+                dlg.setTitle("회원탈퇴");   //창 제목 설정
+                dlg.setMessage("이용해주셔서 감사합니다");     //탈퇴하기 버튼 클릭 시 메시지 창이 뜹니다
                 dlg.setIcon(R.drawable.title);
                 dlg.setPositiveButton("확인", null);
                 dlg.show();

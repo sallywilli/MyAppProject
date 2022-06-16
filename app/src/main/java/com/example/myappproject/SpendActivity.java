15fdd2e0a3f155ab86bb71659716b2ed5c0bb19d
sallywilli <106054193+sallywilli@users.noreply.github.com>
2022-06-16 23:09:15 +0900

    Update SpendActivity.java

--- app/src/main/java/com/example/myappproject/SpendActivity.java
+++ app/src/main/java/com/example/myappproject/SpendActivity.java
@@ -28,7 +28,7 @@ public class SpendActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.spend);
+        setContentView(R.layout.spend);     //spend.xml 화면이 보여짐
         setTitle("소비 기록");
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -39,30 +39,30 @@ public class SpendActivity extends AppCompatActivity {
         btn = findViewById(R.id.btn);
         now = findViewById(R.id.now);
 
-        Calendar sss = Calendar.getInstance();
+        Calendar sss = Calendar.getInstance();     //캘린더
         int cy = sss.get(Calendar.YEAR);
         int cm = sss.get(Calendar.MONTH);
         int cd = sss.get(Calendar.DAY_OF_MONTH);
-        dp.init(cy, cm, cd, new DatePicker.OnDateChangedListener() {
+        dp.init(cy, cm, cd, new DatePicker.OnDateChangedListener() {       //데이트 피커 기능
             @Override
             public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
-                filename = Integer.toString(i)+"_"+Integer.toString(i1+1)+"_"+Integer.toString(i2)+".txt";
+                filename = Integer.toString(i)+"_"+Integer.toString(i1+1)+"_"+Integer.toString(i2)+".txt";      //해당 내용이 텍스트파일에 저장되도록 파일 이름 설정
                 String str = readDiary(filename);
                 et.setText(str);
                 btn.setEnabled(true);
             }
         });
 
-        btn.setOnTouchListener(new View.OnTouchListener() {
+        btn.setOnTouchListener(new View.OnTouchListener() { //버튼 클릭 시 
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 try{
                     FileOutputStream outFs = openFileOutput(filename, Context.MODE_PRIVATE);
                     String str = et.getText().toString();
-                    now.setText("   " + str + "원을 소비했습니다");
+                    now.setText("   " + str + "원을 소비했습니다");     //버튼 클릭과 동시에 입력한 금액과 문구가 화면 윗 부분에 보여집니다
                     outFs.write(str.getBytes());
                     outFs.close();
-                    Toast.makeText(getApplicationContext(), filename+"이 저장됨", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), filename+"이 저장됨", Toast.LENGTH_LONG).show();     //저장 알림 토스트
                 } catch (IOException e){
                     e.printStackTrace();
                 }
@@ -79,10 +79,10 @@ public class SpendActivity extends AppCompatActivity {
             inFs.close();
             String str = new String(txt);
             diaryStr = str.trim();
-            btn.setText("수정하기");
+            btn.setText("수정하기");    //이미 소비 일기를 작성한 날짜를 선택했을 경우 버튼의 문구가 '저장'이 아닌 '수정하기'로 보여짐
         } catch (IOException e){
-            et.setHint("일기 없음");
-            btn.setText("새로 저장");
+            et.setHint("일기 없음");    //작성된 일기가 없는 날짜의 경우 '일기없음'으로 힌트가 보여짐
+            btn.setText("새로 저장");   //작성된 일기가 없는 날짜의 경우 버튼의 문구가 '새로저장'으로 보여짐
         }
         return diaryStr;
     }
